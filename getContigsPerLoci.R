2150ea5b95c6d97e721b511c1c1dac8ee2b72abf
mgabriel01 <marc.gabriel.01@gmail.com>
2017-04-19 11:01:04 +0200

    - genic & antisense contigs (both sharing formerly the same "locus_ID") are now given a separated "locus_ID"
    - bug correction : some contigs could be on a gene in which other genes are included, and that could lead to redundancy with the "locus_ID"
    - function getContigsPerLoci() becomes clusterContigs()

--- getContigsPerLoci.R
+++ getContigsPerLoci.R
@@ -52,111 +52,111 @@ clusterContigs<-function(myContigs,
                          nb_merged_kmers="nb_merged_kmers",
                          nb_cores){
   
-    myContigs<-read.delim(myContigs,sep="\t",header=T)
+  myContigs<-read.delim(myContigs,sep="\t",header=T)
+  
+  myContigs<-myContigs[which(myContigs[is_mapped]==T),]
+  
+  #for each contig, create a new ID with chrs, strand, 5'gene, 3'gene, separated by "&" (for genic or antisense contigs, we just keep sense gene/antisense gene and strand)
+  myContigs$new_ID<-paste(myContigs[,chromosome],myContigs[,strand],myContigs[,gene_5p],myContigs[,gene_3p],sep="&")
+  
+  for(line in 1:nrow(myContigs)){
     
-    myContigs<-mytable[which(mytable[is_mapped]==T),]
-    
-    #for each contig, create a new ID with chrs, strand, 5'gene, 3'gene, separated by "&" (for genic or antisense contigs, we just keep sense gene/antisense gene and strand)
-    myContigs$new_ID<-paste(myContigs[,chromosome],myContigs[,strand],myContigs[,gene_5p],myContigs[,gene_3p],sep="&")
-    
-    for(line in 1:nrow(myContigs)){
+    if(as.character(myContigs[line,EnsemblGeneCol])!="none"){
       
-        if(as.character(myContigs[line,EnsemblGeneCol])!="none"){
-          
-          myContigs[line,"new_ID"]<-paste(myContigs[line,EnsemblGeneCol],myContigs[line,strand],sep="&")
-          
-          
-        }else if(as.character(myContigs[line,Ensembl_AS_GeneCol])!="none"){
-          
-          myContigs[line,"new_ID"]<-paste(myContigs[line,Ensembl_AS_GeneCol],myContigs[line,strand],sep="&")
-          
-        }
+      myContigs[line,"new_ID"]<-paste(myContigs[line,EnsemblGeneCol],myContigs[line,strand],sep="&")
+      
+      
+    }else if(as.character(myContigs[line,Ensembl_AS_GeneCol])!="none"){
+      
+      myContigs[line,"new_ID"]<-paste(myContigs[line,Ensembl_AS_GeneCol],myContigs[line,strand],sep="&")
       
     }
     
-    #unique on the new IDs
-    myIDs<-unique(as.character(myContigs[,"new_ID"]))
+  }
+  
+  #unique on the new IDs
+  myIDs<-unique(as.character(myContigs[,"new_ID"]))
+  
+  #set the number of cores to use
+  if(nb_cores==""){nb_cores=getDoParWorkers()}
+  
+  print(paste("number of cores to use : ",nb_cores,sep=""))
+  
+  registerDoParallel(cores=nb_cores)
+  
+  #retrieve for each ID, all Contigs
+  result<-foreach(i=1:length(myIDs), .combine=rbind) %dopar% {
     
-    #set the number of cores to use
-    if(nb_cores==""){nb_cores=getDoParWorkers()}
+    ContigsOfOneID<-myContigs[which(myContigs["new_ID"]==myIDs[i]),]
     
-    print(paste("number of cores to use : ",nb_cores,sep=""))
+    #number of contigs in the locus
+    nb_contigs<-nrow(ContigsOfOneID)
     
-    registerDoParallel(cores=nb_cores)
+    min_start<-min(ContigsOfOneID[start])
     
-    #retrieve for each ID, all Contigs
-    result<-foreach(i=1:length(myIDs), .combine=rbind) %dopar% {
+    max_end<-max(ContigsOfOneID[end])
+    
+    ContigsOfOneID<-ContigsOfOneID[order(ContigsOfOneID[pvalue],-abs(ContigsOfOneID[log2FC])),]
+    
+    Best_P_value<-ContigsOfOneID[1,pvalue]
+    
+    log2FC_of_Best<-ContigsOfOneID[1,log2FC]
+    
+    meanCond1_of_Best<-ContigsOfOneID[1,grep("mean",value=T,names(ContigsOfOneID))][1]
+    meanCond2_of_Best<-ContigsOfOneID[1,grep("mean",value=T,names(ContigsOfOneID))][2]
+    
+    locus_ID<-ContigsOfOneID[1,"new_ID"]
+    
+    #if the Contigs overlap a sense gene, locus_ID = sense gene ID
+    if(as.character(ContigsOfOneID[1,EnsemblGeneCol])!="none"){
       
-        ContigsOfOneID<-myContigs[which(myContigs["new_ID"]==myIDs[i]),]
-        
-        #number of contigs in the locus
-        nb_contigs<-nrow(ContigsOfOneID)
-        
-        min_start<-min(ContigsOfOneID[start])
-        
-        max_end<-max(ContigsOfOneID[end])
-        
-        ContigsOfOneID<-ContigsOfOneID[order(ContigsOfOneID[pvalue],-abs(ContigsOfOneID[log2FC])),]
-        
-        Best_P_value<-ContigsOfOneID[1,pvalue]
-        
-        log2FC_of_Best<-ContigsOfOneID[1,log2FC]
-        
-        meanCond1_of_Best<-ContigsOfOneID[1,grep("mean",value=T,names(ContigsOfOneID))][1]
-        meanCond2_of_Best<-ContigsOfOneID[1,grep("mean",value=T,names(ContigsOfOneID))][2]
-        
-        locus_ID<-ContigsOfOneID[1,"new_ID"]
-        
-        #if the Contigs overlap a sense gene, locus_ID = sense gene ID
-        if(as.character(ContigsOfOneID[1,EnsemblGeneCol])!="none"){
-          
-            gene_name<-paste(as.character(ContigsOfOneID[1,HugoGeneCol]))
-            
-            locusType<-"genic"
-            
-            #number of kmers in the locus
-            nb_kmers<-sum(as.numeric(as.character(ContigsOfOneID[,nb_merged_kmers])))
-          
-        #if the Contigs don't overlap a sense gene and overlap an antisense gene, locus_ID = antisense gene ID
-        }else if(as.character(ContigsOfOneID[1,EnsemblGeneCol])=="none" & as.character(ContigsOfOneID[1,Ensembl_AS_GeneCol])!="none"){
-          
-            gene_name<-paste(as.character(ContigsOfOneID[1,Hugo_AS_GeneCol]))
-            
-            locusType<-"antisense"
-            
-            #number of kmers in the locus
-            nb_kmers<-sum(as.numeric(as.character(ContigsOfOneID[,nb_merged_kmers])))
-          
-        #if the Contigs don't overlap a sense gene and an antisense gene, they are intergenics, locus_ID = chrs & strand & 5'-gene & 3'-gene
-        }else{
-          
-            #intergenic Contigs may have missing neighbors ; if both are missing, just keep the chromosome
-            locus_ID<-gsub("&none&none$","",locus_ID)
-            
-            gene_name="none"
-            
-            locusType<-"intergenic"
-            
-            nb_kmers<-sum(as.numeric(as.character(ContigsOfOneID[,nb_merged_kmers])))
-          
-        }
-        
-        data.frame(locus_ID=locus_ID,
-                   gene_name=gene_name,
-                   nb_contigs=nb_contigs,
-                   nb_kmers=nb_kmers,
-                   Best_P_value=Best_P_value,
-                   log2FC_of_Best=log2FC_of_Best,
-                   meanCond1_of_Best,
-                   meanCond2_of_Best,
-                   chromosome=as.character(ContigsOfOneID[1,chromosome]),
-                   start=min_start,
-                   end=max_end,
-                   strand=as.character(ContigsOfOneID[1,strand]),
-                   locus_type=locusType,stringsAsFactors= F)
+      gene_name<-paste(as.character(ContigsOfOneID[1,HugoGeneCol]))
+      
+      locusType<-"genic"
+      
+      #number of kmers in the locus
+      nb_kmers<-sum(as.numeric(as.character(ContigsOfOneID[,nb_merged_kmers])))
+      
+    #if the Contigs don't overlap a sense gene and overlap an antisense gene, locus_ID = antisense gene ID
+    }else if(as.character(ContigsOfOneID[1,EnsemblGeneCol])=="none" & as.character(ContigsOfOneID[1,Ensembl_AS_GeneCol])!="none"){
+      
+      gene_name<-paste(as.character(ContigsOfOneID[1,Hugo_AS_GeneCol]))
+      
+      locusType<-"antisense"
+      
+      #number of kmers in the locus
+      nb_kmers<-sum(as.numeric(as.character(ContigsOfOneID[,nb_merged_kmers])))
+      
+    #if the Contigs don't overlap a sense gene and an antisense gene, they are intergenics, locus_ID = chrs & strand & 5'-gene & 3'-gene
+    }else{
+      
+      #intergenic Contigs may have missing neighbors ; if both are missing, just keep the chromosome
+      locus_ID<-gsub("&none&none$","",locus_ID)
+      
+      gene_name="none"
+      
+      locusType<-"intergenic"
+      
+      nb_kmers<-sum(as.numeric(as.character(ContigsOfOneID[,nb_merged_kmers])))
+      
     }
     
-    return(result)
+    data.frame(locus_ID=locus_ID,
+               gene_name=gene_name,
+               nb_contigs=nb_contigs,
+               nb_kmers=nb_kmers,
+               Best_P_value=Best_P_value,
+               log2FC_of_Best=log2FC_of_Best,
+               meanCond1_of_Best,
+               meanCond2_of_Best,
+               chromosome=as.character(ContigsOfOneID[1,chromosome]),
+               start=min_start,
+               end=max_end,
+               strand=as.character(ContigsOfOneID[1,strand]),
+               locus_type=locusType,stringsAsFactors= F)
+  }
+  
+  return(result)
 }
 
 #example
