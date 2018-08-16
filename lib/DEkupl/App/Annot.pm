3471cdafb5179e8f34a6b3105a7106a8e42ffac1
jaudoux <jerome.audoux@gmail.com>
2018-08-16 16:08:42 +0200

    Print error message if output director cannot be created

--- lib/DEkupl/App/Annot.pm
+++ lib/DEkupl/App/Annot.pm
@@ -75,7 +75,7 @@ sub BUILD {
   }
 
   # Create output dir
-  mkdir $output_dir;
+  mkdir $output_dir or die "Cannot create the output directory";
 
   # Find files from index
   # TODO we should have a class for that
