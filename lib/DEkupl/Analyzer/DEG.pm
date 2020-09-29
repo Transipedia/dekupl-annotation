11d0b6219b962f418b3a4c230fae55ab115f9f40
aLaine1 <48355209+aLaine1@users.noreply.github.com>
2020-09-29 13:41:32 +0200

    is_diff : 1,-1,0 instead of TRUE/FALSE

--- lib/DEkupl/Analyzer/DEG.pm
+++ lib/DEkupl/Analyzer/DEG.pm
@@ -67,7 +67,7 @@ sub BUILD {
   while(my $contig = $contigs_it->()) {
     if(defined $contig->{gene_id}) {
       my $is_diff = $gene_is_diff{$contig->{gene_id}};
-      $contig->{gene_is_diff} = DEkupl::Utils::booleanEncoding($is_diff) if defined $is_diff;
+      $contig->{gene_is_diff} = $is_diff if defined $is_diff;
       $nb_contigs_with_diff_gene++ if $is_diff;
       # Save contig
       $self->contigs_db->saveContig($contig);
